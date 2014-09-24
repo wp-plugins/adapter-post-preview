257e44f025ab84b8f9e4cab0359277e87d54bf3f
ryankienstra <ryankienstra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-24 02:47:24 +0000

    First tag for Adapter Post Preview: 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/adapter-post-preview/tags/1.0.0@995831 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-app-carousel.php
@@ -0,0 +1,72 @@
+<?php
+
+// Builds and echoes a modal carousel for each gallery 
+class APP_Carousel {
+
+  private $carousel_id ;
+  private $carousel_inner_items ;
+  private $number_of_inner_items ;  
+  private $carousel_indicators ;
+  private $slide_to_index ;
+  private $post_markup ;
+  private static $instance_id = 1 ;                                                                                                                  
+  public function __construct() {
+    $this->carousel_id = 'appw-carousel-' . self::$instance_id ;
+    $this->carousel_inner_items  = '' ;
+    $this->number_of_inner_items = 0 ; 
+    $this->carousel_indicators = '' ;
+    $this->slide_to_index = 0 ;
+    self::$instance_id++;       
+  }
+    
+  public function add_post_markup( $post_markup ) {
+    $this->append_post_markup_to_inner_items( $post_markup ) ; 
+    $this->append_to_carousel_indicators() ;
+    $this->number_of_inner_items++ ; 
+  }
+
+  private function append_post_markup_to_inner_items( $post_markup ) {
+    $is_active = (0 == $this->slide_to_index ) ? 'active' : '' ;
+    $this->carousel_inner_items .= 
+    "<div class='item {$is_active}'>
+      {$post_markup}
+    </div> \n" ;
+  }
+
+  private function append_to_carousel_indicators(){
+    $is_active = (0 == $this->slide_to_index ) ? 'active' : '' ;
+    
+    $this->carousel_indicators .= 
+    "<li class='{$is_active}' data-target='#{$this->carousel_id}' data-slide-to='{$this->slide_to_index}' ></li>" ;
+    $this->slide_to_index++ ;
+  }
+
+  private function maybe_get_controls() {
+    if ( $this->number_of_inner_items > 1 ) {
+       return "<a class='left carousel-control' href='#{$this->carousel_id}' data-slide='prev'><span class='glyphicon glyphicon-chevron-left'></span></a>
+               <a class='right carousel-control' href='#{$this->carousel_id}' data-slide='next'><span class='glyphicon glyphicon-chevron-right'></span></a>" ; 
+    }
+  }
+
+  private function maybe_get_indicators() {
+    if ( $this->number_of_inner_items > 1 ) {
+      return "<ol class='carousel-indicators'> 
+	       {$this->carousel_indicators}
+              </ol>\n" ;
+    }
+  }
+  
+  public function get() {
+    $controls = $this->maybe_get_controls() ;
+    $indicators = $this->maybe_get_indicators() ; 
+    return " <!-- carousel -->
+      <div id='{$this->carousel_id}' class='carousel slide'>	        
+        {$indicators}
+	<!-- Posts -->
+	<div class='carousel-inner'>      
+	   {$this->carousel_inner_items}
+	</div>
+	{$controls}
+      </div>  <!-- .carousel --> \n" ; 
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Adapter Post Preview ===
+Contributors: ryankienstra
+Donate link: http://jdrf.org/get-involved/ways-to-donate/
+Tags: widgets, post, Bootstrap, mobile, responsive, 
+Requires at least: 3.8
+Tested up to: 4.0 
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A widget with a post preview, or a carousel of the most recent posts.
+
+== Description ==
+
+* Creates a widget with the post's featured image, headline, excerpt, and link.
+* To see the carousel of posts, you must have Twitter Bootstrap 3 and Glyphicons.
+* Hides the widget if the post is a single post on the page. For example, if you are on the "Hello World" page, you won't see the widget with a preview of "Hello World."
+* The carousel won't show posts that don't have an image.
+
+== Installation ==
+
+1. Upload the adapter-post-preview directory to your /wp-content/plugins directory.
+1. In the "Plugins" menu, find "Adapter Post Preview," and click "Activate."
+1. Add a "Post Preview" widget by going to the admin menu and clicking "Appearance" > "Widgets"
+1. Select the post you want. You must have Bootstrap to use the carousel.
+
+== Frequently Asked Questions ==
+
+= What does this require? =
+
+The carousel of recent posts requires Twitter Bootstrap 3 with Glyphicons. 
+
+= How can I change the text in the post link? =
+
+Put the following in your functions.php file:
+`apply_filters( 'appw_link_text' , 'my_appw_link_text' ) ;
+ function my_appw_link_text( $text ) {
+   return 'Keep reading' ; // or your own text
+ }`
+ 
+== Screenshots ==
+
+1. A "Post Preview" widget in the sidebar.
+2. A carousel of recent posts, which requires Bootstrap 3.
+
+== Changelog ==
+
+= 1.0.0 =
+* First version
+
+== Upgrade Notice ==
+
+* N/A, first version
