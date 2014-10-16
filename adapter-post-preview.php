0ac6416e6757f3de786aeb6da420600a61cbd30e
ryankienstra <ryankienstra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-16 01:22:16 +0000

    Version 1.0.1 : Slight styling change to the carousel to support IE.
    git-svn-id: https://plugins.svn.wordpress.org/adapter-post-preview/tags/1.0.1@1008308 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adapter-post-preview.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Plugin Name: Adapter Post Preview
+Plugin URI: www.ryankienstra.com/plugins/adapter-post-preview
+Description: Post preview widget
+
+Version: 1.0.1
+Author: Ryan Kienstra
+Author URI: www.ryankienstra.com
+License: GPL2
+*/
+
+define( 'APPW_PLUGIN_SLUG' , 'adapter-post_preview' ) ; 
+define( 'APPW_PLUGIN_VERSION' , '1.0.1' ) ;
+
+load_plugin_textdomain( 'adapter-post-preview' , false , basename( dirname( __FILE__ ) ) . '/languages' ) ;
+
+add_action( 'plugins_loaded' , 'appw_get_included_files' ) ;
+function appw_get_included_files() {
+  include_once( plugin_dir_path( __FILE__ ) . 'includes/class-app-carousel.php' ) ;
+  include_once( plugin_dir_path( __FILE__ ) . 'includes/class-adapter-post-widget.php' ) ;  
+}
+
+add_action( 'widgets_init' , 'appw_register_widget' ) ;
+function appw_register_widget() {
+  register_widget( 'Adapter_Post_Widget' ) ;  
+}
+
+add_action( 'wp_enqueue_scripts' , 'appw_enqueue_stylesheet' ) ;
+function appw_enqueue_stylesheet() { 
+  wp_enqueue_style( APPW_PLUGIN_SLUG . '-style', plugins_url( '/css/app-style.css' , __FILE__ ) , array() , APPW_PLUGIN_VERSION ) ;
+}
